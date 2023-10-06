5e266012a1ee5e2e446036fecf2105538ed1be01
Manohar Gowda <110281915+Manohar-gowda@users.noreply.github.com>
2023-10-06 22:17:45 +0530

    Add files via upload

---
+++ Resume-Builder/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '844db8f31c978fd1021e86c9b3f82e099fdef681',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '844db8f31c978fd1021e86c9b3f82e099fdef681',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
